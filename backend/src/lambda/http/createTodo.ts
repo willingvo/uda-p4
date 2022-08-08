654a2b555ca308356f1ad8e5438fc5f2e208c5f6
NguyenVo <NguyenVo@ge.com>
2022-08-08 12:22:34 +0700

    Fix bug: json schema, add xray screenshot

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -12,17 +12,6 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
   const userId = getUserId(event)
-  if (!newTodo.name) return {
-    statusCode: 500,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
-    },
-    body: JSON.stringify({
-      
-    })
-  }
-  
   const item = await createTodo(userId, newTodo)
   return {
     statusCode: 201,
